fdb7aa5b9ef3e3a5f1e9237cb194d444a2313f58
Magnus Åsrud <magnusasrud92@gmail.com>
2015-09-16 21:11:48 +0200

    Refractor js

--- src/script.js
+++ src/script.js
@@ -1,35 +1,30 @@
 /*jshint esnext: true */
 const canvas = document.getElementById("clock"); // get canvas
 const ctx = canvas.getContext("2d");
-const dec = 45, radius = 20, pi2 = Math.PI * 2; // dec = diameter + 5px
+const dec = 45, radius = 20, pi2 = Math.PI * 2;	 // dec = diameter + 5px
 const color = ["rgba(200, 0, 0, 0.5)", "rgba(0, 200, 0, 0.5)", "rgba(0, 0, 200, 0.5)"]; // red, green, blue
 
 /* Draws a binary representation of the current time on the canvas. */
 function updateClock() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Reset canvas.
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 		// Reset canvas.
 
-    const date = new Date(); // Get localtime
-    var tmp = 0;
+    const date = new Date(); 								// Get localtime
 
-    var time = [date.getHours(), date.getMinutes(), date.getSeconds()]; // HH:MM:SS
-    time = time.map(obj => [Math.floor(obj / 10), obj % 10]);           // MM -> [M, M] etc (28 -> [2, 8])
-    time.forEach(obj => {
-    	for (y = 0; y < 4; y++) { // Draw the 4*2 dots (HH then MM then SS). Draws the LSB first from the bottom.
-        	draw((((obj[0] >> 3-y)) & 1), 2*tmp, y, color[tmp]);		// Largest number   eks: 2
-        	draw((((obj[1] >> 3-y)) & 1), 2*tmp+1, y, color[tmp]);		// Smallest number       8
-    	}
-        tmp++;
-    });
+    [date.getHours(), date.getMinutes(), date.getSeconds()]	// Get HH:MM:SS
+        .map(obj => [Math.floor(obj / 10), obj % 10])		// Split HH into [H,H] etc
+    	.reduce((a, b) => a.concat(b))						// Flatten the array [[H,H],... -> [H, H, M...
+        .forEach((obj, index) => draw(obj, index, color[Math.floor(index/2)])); // Draw the number on canvas
 }
 
 /* Draw a circle on the canvas */
-function draw(val, x, y, color) {
-	if (val !== 0) {
-        ctx.beginPath();
-       	ctx.fillStyle = color;
-       	ctx.arc(x*dec+20, y*dec+20, radius, 0, pi2);
-       	ctx.closePath();
-     	ctx.fill();
+function draw(val, col, color) {
+    for (var row = 0; row < 4; ++row) { 					// For each of the 4 bits...
+    	if ((val >> 3-row & 1) === 0) continue;				// (The bit is zero, nothing to draw)
+    	ctx.beginPath();									// Draw a circle
+    	ctx.fillStyle = color;
+    	ctx.arc(col*dec+20, row*dec+20, radius, 0, pi2);
+    	ctx.closePath();
+    	ctx.fill();
     }
 }
 
